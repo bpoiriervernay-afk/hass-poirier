b811613975ff67adb8f119cef46a399954b486f5
bpoiriervernay-afk <bpoiriervernay@gmail.com>
2026-01-19 17:29:05 +0100

    Implement async setup for web scrapper component

---
+++ custom_components/web_scrapper/__init__.py
@@ -0,0 +1,15 @@
+from .const import DOMAIN
+from .sensor import WebScraperCoordinator
+
+async def async_setup_entry(hass, entry):
+    hass.data.setdefault(DOMAIN, {})
+
+    coordinator = WebScraperCoordinator(hass, entry)
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(
+        entry, ["sensor", "button"]
+    )
+    return True
