cea118e29c238fb1f7472723a5b663e08e6e6373
bpoiriervernay-afk <bpoiriervernay@gmail.com>
2026-01-19 17:30:15 +0100

    Add constants for web scraper configuration

---
+++ custom_components/web_scrapper/const.py
@@ -0,0 +1,10 @@
+DOMAIN = "web_scraper"
+
+CONF_NAME = "name"
+CONF_URL = "url"
+CONF_SELECTOR = "selector"
+CONF_SELECTOR_TYPE = "selector_type"
+CONF_REGEX = "regex"
+CONF_SCAN_INTERVAL = "scan_interval"
+
+DEFAULT_SCAN_INTERVAL = 300  # secondes
